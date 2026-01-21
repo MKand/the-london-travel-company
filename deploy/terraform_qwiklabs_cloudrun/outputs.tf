cff1824e2680e126bf5e9fee449914dfc979aa7d
MKand <manasakandula.m@gmail.com>
2026-01-21 19:42:29 +0100

    added localdb and qwiklabs

---
+++ deploy/terraform_qwiklabs_cloudrun/outputs.tf
@@ -0,0 +1,3 @@
+output "ltc_address" {
+  value = google_cloud_run_v2_service.app.uri
+}
---
+++ deploy/terraform_qwiklabs_cloudrun/variables.tf
@@ -0,0 +1,58 @@
+variable "gcp_project_id" {
+  type        = string
+  description = "GCP Project ID"
+}
+
+variable "repo_prefix" {
+  type        = string
+  description = "Docker/Artifact registry prefix"
+  default     = "us-central1-docker.pkg.dev/o11y-movie-guru/london-travel-agency"
+}
+
+variable "image_tag" {
+  description = "TAG of the london travel agent docker images"
+  default     = "obslab-v4"
+}
+
+variable "gcp_region" {
+  type        = string
+  default     = "us-central1"
+  description = "Region"
+}
+
+# Default value passed in
+variable "gcp_zone" {
+  type        = string
+  description = "Zone to create resources in."
+  default     = "us-central1-c"
+}
+
+variable "vertexAI_model_location" {
+  type        = string
+  description = "Region from which the vertexAI models are called."
+  default     = "us-central1"
+}
+
+
+variable "branch_name" {
+  type        = string
+  description = "value of the branch for cloud build trigger"
+  default     = "main"
+}
+
+variable "otel_file" {
+  type = string
+
+  description = "URL of the otel config"
+  default     = "https://raw.githubusercontent.com/MKand/movie-guru/refs/heads/main/utils/metrics/otel.values.yaml"
+}
+variable "db_file" {
+  type = string
+
+  description = "URL of the sqllite db file"
+  default     = "https://raw.githubusercontent.com/MKand/the-london-travel-company/main/data_london/london_travel.sql"
+}
+
+variable "service_account_key_file" {
+  type = string
+}
