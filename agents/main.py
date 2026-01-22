2b42899ec4935c950ea26cad0e7365e35ba06fc7
MKand <manasakandula.m@gmail.com>
2026-01-22 01:36:17 +0100

    removed extra otel config

--- agents/main.py
+++ agents/main.py
@@ -15,27 +15,7 @@
 import os
 import uvicorn
 import logging
-import google.auth
-import google.auth.transport.requests
-import grpc
-from google.auth.transport.grpc import AuthMetadataPlugin
-from opentelemetry import _events as events, _logs as logs, metrics, trace
 from google.cloud import logging as cloud_logging
-from opentelemetry.exporter.cloud_logging import CloudLoggingExporter
-from opentelemetry.exporter.cloud_monitoring import CloudMonitoringMetricsExporter
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
-    OTLPSpanExporter,
-)
-from opentelemetry.instrumentation.google_genai import GoogleGenAiSdkInstrumentor
-from opentelemetry.sdk._events import EventLoggerProvider
-from opentelemetry.sdk._logs import LoggerProvider
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.instrumentation.google_genai import GoogleGenAiSdkInstrumentor
 from google.adk.cli.fast_api import get_fast_api_app
 from fastapi import FastAPI
 from london_agent.sub_agents.search_agent.tools import setup_sqlite_client
@@ -55,84 +35,7 @@ logger = logging.getLogger(__name__)
 
 AGENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_ORIGINS = ["*"]
-SESSION_DB_URL = "sqlite:///./sessions.db"
 
-def str_to_bool(s: str) -> bool:
-    """
-    Convert a string to a boolean.
-    Accepts: yes/no, true/false, t/f, y/n, on/off, 1/0.
-    Raises ValueError if invalid.
-    """
-    truthy = {"y", "yes", "t", "true", "on", "1"}
-    falsy  = {"n", "no", "f", "false", "off", "0"}
-
-    s = s.strip().lower()
-    if s in truthy:
-        return True
-    if s in falsy:
-        return False
-    raise ValueError(f"Invalid boolean string: {s}")
-
-
-USE_MIDDLEWARE = str_to_bool(os.getenv("USE_MIDDLEWARE", "False"))
-
-GCP_SCOPES = [
-    "https://www.googleapis.com/auth/trace.append",       # For Cloud Trace
-    "https://www.googleapis.com/auth/logging.write",      # For Cloud Logging
-    "https://www.googleapis.com/auth/monitoring.write",   # For Cloud Monitoring
-    "https://www.googleapis.com/auth/cloud-platform",     # Broad scope for other Google ADK/AI Platform needs
-   ]
-
-def setup_opentelemetry() -> None:
-    credentials, project_id = google.auth.default(scopes=GCP_SCOPES)
-    resource = Resource.create(
-        attributes={
-            SERVICE_NAME: "lta-sa",
-            "gcp.project_id": project_id,
-        }
-    )
-    # Set up OTLP auth
-    request = google.auth.transport.requests.Request()
-    auth_metadata_plugin = AuthMetadataPlugin(credentials=credentials, request=request)
-
-    channel_creds = grpc.composite_channel_credentials(
-        grpc.ssl_channel_credentials(),
-        grpc.metadata_call_credentials(auth_metadata_plugin),
-    )
-
-    # Set up OpenTelemetry Python SDK
-    tracer_provider = TracerProvider(resource=resource)
-    tracer_provider.add_span_processor(
-        BatchSpanProcessor(
-            OTLPSpanExporter(
-                credentials=channel_creds,
-                endpoint="https://telemetry.googleapis.com:443/v1/traces",
-            )
-        )
-    )
-    trace.set_tracer_provider(tracer_provider)
-
-    logger_provider = LoggerProvider(resource=resource)
-    logger_provider.add_log_record_processor(
-        BatchLogRecordProcessor(CloudLoggingExporter())
-    )
-
-    logs.set_logger_provider(logger_provider)
-
-    event_logger_provider = EventLoggerProvider(logger_provider)
-    events.set_event_logger_provider(event_logger_provider)
-
-    reader = PeriodicExportingMetricReader(CloudMonitoringMetricsExporter())
-    meter_provider = MeterProvider(metric_readers=[reader], resource=resource)
-    metrics.set_meter_provider(meter_provider)
-
-    # Load instrumentors
-    # TODO: if this is too chatty because of ADK's use of SQL for session management, the
-    # connection used in tools.py can be instrumented to cut ADK spans out
-    GoogleGenAiSdkInstrumentor().instrument()
-    return
-
-setup_opentelemetry()
 setup_sqlite_client()
 
 # Call the function to get the FastAPI app instance
@@ -140,22 +43,8 @@ app: FastAPI = get_fast_api_app(
     agents_dir=AGENT_DIR,
     allow_origins=ALLOWED_ORIGINS,
     web=True,
+    trace_to_cloud=True,
 )
-
-# This middleware is used to trigger errors intentionally 
-@app.middleware("http")
-async def add_middleware(request, call_next):
-    try:
-        if USE_MIDDLEWARE:
-            # purposefully unused
-            additional_props = os.environ['ADDITIONAL_PROPS']
-            logger.info("Middleware triggered")
-            # do nothing
-        response = await call_next(request)
-        return response
-    except Exception as e:
-        logger.exception(logger.error(e, stack_info=True, exc_info=True))
-    
 
 @app.get("/health")
 async def read_root():
