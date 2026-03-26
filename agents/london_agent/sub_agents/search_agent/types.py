697791fc90d6e9fcccb3d3a214dada8dea84b4c6
MKand <manasakandula.m@gmail.com>
2026-03-26 10:43:11 +0100

    added search result output type

--- agents/london_agent/sub_agents/search_agent/types.py
+++ agents/london_agent/sub_agents/search_agent/types.py
@@ -2,18 +2,18 @@ from pydantic import BaseModel, Field
 from typing import List
  
 class Item(BaseModel):
-    name: str = Field(description="The name of the item.")
-    description: str = Field(description="The description of the item.")
+    name: str = Field(description="The name of the item.", default="some name")
+    description: str = Field(description="The description of the item.", default="some description")
 
 class SearchResult(BaseModel):
     items: List[Item] = Field(description="The list of items found.", default=[])
     
 
 class Activity(Item):
-    duration: str = Field(description="The duration of the activity in natural language eg 2 hours.")
-    cost: str = Field(description="The cost of the activity in natural language eg Free or 20 pounds.")
-    kid_friendly: str = Field(description="Whether the activity is kid friendly eg Yes or No.")
+    duration: str = Field(description="The duration of the activity in natural language eg 2 hours.", default="1 hr")
+    cost: str = Field(description="The cost of the activity in natural language eg Free or 20 pounds.", default="free")
+    kid_friendly: str = Field(description="Whether the activity is kid friendly eg Yes or No.", default="no")
     
 class Location(Item):
-    category: str = Field(description="The category of the location.")
+    category: str = Field(description="The category of the location.", default="some category")
 
